65b31539ccadefd45bfa1e132ebaa7b36129892a
Viktor Nareiko <vnareiko.lt@gmail.com>
2021-10-06 09:56:54 +0300

    Init template

---
+++ __tests__/add.test.js
@@ -0,0 +1,7 @@
+import { add } from '../index';
+
+it('Adds two numbers', () => {
+    const result = add(2, 5);
+
+    expect(result).toEqual(7);
+});
---
+++ index.js
@@ -0,0 +1,5 @@
+const add = (l, r) => {
+    return l + r;
+}
+
+export { add };
